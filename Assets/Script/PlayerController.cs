247600e595afda52edd999fb72f714da318dedf5
Isma <ismaele3000@gmail.com>
2023-02-15 17:57:42 +0100

    Lezione 15/02

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,20 +4,69 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField][Range(0,10)]float Speed = 5f;
+    [SerializeField] [Range(0, 10)] float Speed = 5f;
+    [SerializeField] [Range(0, 100)] float Jump = 5f;
+    [SerializeField] Rigidbody Body;
+    [SerializeField] LayerMask GroundMask;
 
+    int score = 0;
+    bool isGrounded = true;
+
+    
     void Start()
     {
-
+        Body = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
 
-        float XMove = Input.GetAxis("Horizontal");
-        Debug.Log(XMove);
+        float XMove = Input.GetAxisRaw("Horizontal");
+        float ZMove = Input.GetAxisRaw("Vertical");
 
-        Vector3 playerMovement = Vector3.right * XMove * Speed * Time.deltaTime;
-        transform.position += playerMovement;
+        Vector3 playerMovement = (Vector3.right * XMove + Vector3.forward * ZMove).normalized * Speed;
+        playerMovement.y = Body.velocity.y;
+        isGrounded = Physics.Raycast(transform.position, -transform.up, 1.1f, GroundMask);
+        Debug.DrawRay(transform.position, -transform.up * 1.1f, Color.cyan);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            playerMovement.y += Mathf.Sqrt(Jump * -2f * (-9.81f));
+            //Body.AddForce(Vector3.up * Jump, ForceMode.Impulse);
+        }
+
+        Body.velocity = playerMovement;
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.transform.name);
+   
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            score += 1;
+            Debug.Log(score);
+            Destroy(other.gameObject);
+        }
+        /*if (other.gameObject.CompareTag("Ground"))
+            isGrounded = true;*/
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        /*if (other.gameObject.CompareTag("Ground"))
+            isGrounded = false;*/
+    }
+     
+
 }
